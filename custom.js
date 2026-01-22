164ca7b0b9c7e949233df93c53d9d3a15be4db5d
Levansarajishvili <levan.sarajishvili123@gmail.com>
2026-01-22 17:19:54 +0400

    createUserCard

--- custom.js
+++ custom.js
@@ -25,3 +25,29 @@ async function getRepos(username) {
     }
 }
 
+function createUserCard(user) {
+    const cardHTML = `
+    <div class="card">
+    <div>
+    <img src="${user.avatar_url}" alt="${user.name}" class="avatar">
+    </div>
+
+    <div class="user-info">
+    <h2>${user.name}</h2>
+    <p>${user.bio}</p>
+    <ul>
+    <li>${user.followers} <strong>followers</strong></li>
+    <li>${user.following} <strong>following</strong></li>
+    <li>${user.public_repos} <strong>Repos</strong></li>
+    </ul>
+
+    <div id="repos"></div>
+    </div>
+    </div>
+    `
+
+    main.innerHTML = cardHTML
+}
+
+
+
